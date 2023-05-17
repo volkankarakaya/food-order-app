9f3a9576ac2c8632c0ddfe9ee41965b3a1739356
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-05-17 16:02:35 +0300

    add showCart hideCart click functionality

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
     return <Fragment>
         <header className={classes.header} >
             <h1>ReactMeals</h1>
-            <HeaderCartButton>Cart</HeaderCartButton>
+            <HeaderCartButton onClick={props.onShowCart}>Cart</HeaderCartButton>
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="meals on the table" />
