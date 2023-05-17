d88677740efd90be78a77893755f3bc779ac16c8
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-05-17 15:16:53 +0300

    add modal component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./components/Layout/Header";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false)
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,27 +1,29 @@
-import classes from "./Cart.module.css";
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
 
 const Cart = (props) => {
   const cartItems = (
-    <ul>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
   );
 
   return (
-    <div>
-        {cartItems}
+    <Modal>
+      {cartItems}
       <div className={classes.total}>
-        <div>Total Amount</div>
-        <div>35.62</div>
+        <span>Total Amount</span>
+        <span>35.62</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
 export default Cart;
+
